c4c6a1068da7807661d94d4c8cf7d0aa7f4d49a0
Amardeep Ganguly <amardeepganguly@Amardeeps-MacBook-Air.local>
2024-01-31 22:39:38 +0530

    add-space-grotesk

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const inter = Inter({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
-const spaceGrotesk = Inter({
+const spaceGrotesk = Space_Grotesk({
   variable: "--font-spaceGrotesk",
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
